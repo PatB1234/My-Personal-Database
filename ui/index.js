209d5782a5d3898492918176709febf9c8d995a3
PatB1234 <80209204+PatB1234@users.noreply.github.com>
2021-12-29 15:09:01 +0300

    Added some customisation an login to the portal in the form of a welcome and a datetime text and added a button to retrive management tasks.

--- ui/index.js
+++ ui/index.js
@@ -83,4 +83,40 @@ function retriveManagementTask() {
     });
 }
 
-retriveManagementTask()
+var today = new Date();
+var day;
+
+switch (today.getDay() + 1) {
+
+    case 1:
+
+        day = "Sunday";
+        break;
+    case 2:
+
+        day = "Monday";
+        break;
+    case 3:
+
+        day = "Tuesday";
+        break;
+    case 4:
+
+        day = "Wednesday";
+        break;
+    case 5:
+
+        day = "Thursday";
+        break;
+    case 6: 
+
+        day = "Friday";
+        break;
+    case 7:
+
+        day = "Saturday";
+        break;
+}
+
+var date = `${day} ${today.getDate()}-${(today.getMonth()+1)}-${today.getFullYear()}`;
+document.getElementById("datetimeh6").innerText = date;
